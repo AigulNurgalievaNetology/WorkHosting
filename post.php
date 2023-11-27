641525f96446edbc1f7819bf9454b15ee5d392c3
AigulNurgalievaNetology <145849349+AigulNurgalievaNetology@users.noreply.github.com>
2023-11-27 20:20:20 +0300

    Delete post.php
    Delete post.php

--- post.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_POST['name'])) {
-  $_SESSION['name'] = $_POST['name'];
-  echo 'Добавлен новый пользователь ' . $_SESSION['name'];
-  echo "<br><a href='index.php'>Вернуться в главное окно</a><br>";
-}
-?>
